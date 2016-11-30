52c8837144f97ed033473980c96d49baba03e87c
ymca85 <bhushan.ymca85@gmail.com>
2016-11-30 18:32:41 +0530

    green-corrected logic as per scenario for two different items

--- src/ItemPriceCalculator.java
+++ src/ItemPriceCalculator.java
@@ -2,7 +2,7 @@
 public class ItemPriceCalculator {
 
 	public int calculateItemPrice(int itemPrice, int numberOfItems, int priceOfSecondItem, int quantityOfSecondItem) {
-		return itemPrice * numberOfItems;
+		return itemPrice * numberOfItems + priceOfSecondItem * quantityOfSecondItem;
 	}
 
 }
