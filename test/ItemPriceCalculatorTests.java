b12e99442e1ca143d0be4ab7072f4dd4969d1dc9
ymca85 <bhushan.ymca85@gmail.com>
2016-11-30 18:09:40 +0530

    green-added test for calculating prices for more items

--- test/ItemPriceCalculatorTests.java
+++ test/ItemPriceCalculatorTests.java
@@ -11,4 +11,18 @@ public class ItemPriceCalculatorTests {
 		assertTrue(itemPriceCalculator.calculateItemPrice(100, 1) == 100);
 	}
 	
+	@Test
+	public void test_calculate_price_of_two_items() {
+
+		ItemPriceCalculator itemPriceCalculator = new ItemPriceCalculator();
+		assertTrue(itemPriceCalculator.calculateItemPrice(100, 2) == 200);
+	}
+	
+	@Test
+	public void test_calculate_price_of_ten_items() {
+
+		ItemPriceCalculator itemPriceCalculator = new ItemPriceCalculator();
+		assertTrue(itemPriceCalculator.calculateItemPrice(100, 10) == 1000);
+	}
+	
 }
